acd8ff55480713d92da2b1a949fce2d8bcbb852e
zyenapz <zyenapz@gmail.com>
2022-07-22 00:04:12 +0800

    Improved annotations, and renamed a variable

--- 1. Dot Product.py
+++ 1. Dot Product.py
@@ -15,6 +15,8 @@ FPS = 60
 my_font = pygame.font.SysFont('Comic Sans MS', 16)
 
 # Actor class
+
+
 class Actor:
 
     def __init__(self, x, y, color, other, radius=10):
@@ -30,36 +32,46 @@ class Actor:
             pygame.draw.line(screen, "white", self.pos, self.other.pos)
 
 # Some maths
+
+
 def normalize(val, mincap, maxcap):
     return (val - mincap) / (maxcap - mincap)
 
+
 def calc_conesize(nt, a):
     # nt - normalized visibility threshold
-    # a - alpha (view sensitivity)
+    # a - alpha (the higher it is, the larger the cone is)
     return ((360 - (nt * 360)) / 2) + (nt * a)
+
 
 def clip(val, mincap, maxcap):
     if val < mincap:
         return mincap
     elif val > maxcap:
         return maxcap
-        
+
     return val
+
 
 # Visibility constants
 VISIBLE_THRESHOLD = 0.7
 VISIBLE_DISTANCE = 120
-VIEW_SENSITIVITY = 2 # This is an arbitrary number, play around with it!
+
+# This is an arbitrary number, play around with it!
+# The higher it is, the larger the cone is ...
+# ... This has the consequence of the cone appearing "larger" than the actual threshold
+CONE_ALPHA = 18
 
 # Create actors
-cursor = Actor(50, 50, "yellow", None, 5) 
-alice = Actor(300, 200, "green", None) # 
-bob = Actor(300, 150, "red", cursor) # Bob's viewing direction is towards the cursor's position
+cursor = Actor(50, 50, "yellow", None, 5)
+alice = Actor(300, 200, "green", None)
+# Bob's viewing direction is towards the cursor's position
+bob = Actor(300, 150, "red", cursor)
 
 while is_running:
 
     clock.tick(FPS)
-    
+
     # Get input -----------------------------------------------
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
@@ -85,41 +97,45 @@ while is_running:
         bob.pos.x -= 1
     if kp[pygame.K_d]:
         bob.pos.x += 1
-    
-    # Mouse input   
+
+    # Mouse input
     m_pos = pygame.mouse.get_pos()
     m_pos = pygame.Vector2(m_pos[0], m_pos[1])
 
     # Update -----------------------------------------------
-    try: # Lazy fix for calculating dot product using 0
+    try:  # Lazy fix for calculating dot product using 0
         cursor.pos = m_pos
-        bob_cursor = (bob.pos - cursor.pos).normalize() # bob-cursor vector
-        bob_alice = (bob.pos - alice.pos).normalize() # bob-alice vector
+        bob_cursor = (bob.pos - cursor.pos).normalize()  # bob-cursor vector
+        bob_alice = (bob.pos - alice.pos).normalize()  # bob-alice vector
         dot_product = bob_cursor.dot(bob_alice)
-        dist_bc = bob.pos.distance_to(alice.pos)
+        dist_bob_alice = bob.pos.distance_to(alice.pos)
     except:
         pass
 
     # Calculate angle of bob's position with respect to the cursor's position
     rel_y = cursor.pos.y - bob.pos.y
-    rel_x = cursor.pos.x - bob.pos.x 
+    rel_x = cursor.pos.x - bob.pos.x
     radians = math.atan2(rel_y, rel_x)
     degrees = (radians / math.pi) * 180
 
     # Draw -----------------------------------------------
-        
+
     screen.fill("black")
 
     # Draw view cone
     size = VISIBLE_DISTANCE * 2
     viewcone = Image.new("RGBA", (size, size))
-    cone_size = calc_conesize(normalize(VISIBLE_THRESHOLD, -1, 1), VIEW_SENSITIVITY)
-    start = degrees - cone_size # PIL pie slices starts from 3 o'clock, and goes clockwise
+    cone_size = calc_conesize(
+        normalize(VISIBLE_THRESHOLD, -1, 1), CONE_ALPHA)
+    # PIL pie slices starts from 3 o'clock, and goes clockwise
+    start = degrees - cone_size
     end = degrees + cone_size
-    ImageDraw.Draw(viewcone).pieslice((0, 0, size-1, size-1), start, end, fill="blue")
+    ImageDraw.Draw(viewcone).pieslice(
+        (0, 0, size-1, size-1), start, end, fill="blue")
 
     # Convert PIL image to pygame image
-    viewcone_img = pygame.image.fromstring(viewcone.tobytes(), viewcone.size, viewcone.mode)
+    viewcone_img = pygame.image.fromstring(
+        viewcone.tobytes(), viewcone.size, viewcone.mode)
     viewcone_rect = viewcone_img.get_rect(center=bob.pos)
 
     # Draw objects
@@ -129,22 +145,26 @@ while is_running:
     cursor.draw(screen)
 
     # Draw texts
-    dot_text = my_font.render(f"Dot: {round(dot_product, 3)}, THRESH: {round(VISIBLE_THRESHOLD, 3)}", False, (255, 255, 255))
-    dist_text = my_font.render(f"Distance (Bob-Alice): {round(dist_bc, 3)}", False, (255, 255, 255))
+    dot_text = my_font.render(
+        f"Dot: {round(dot_product, 3)}, THRESH: {round(VISIBLE_THRESHOLD, 3)}", False, (255, 255, 255))
+    dist_text = my_font.render(
+        f"Distance (Bob-Alice): {round(dist_bob_alice, 3)}", False, (255, 255, 255))
     instr1_text = my_font.render(f"WASD = move Bob", False, (0, 255, 255))
-    instr2_text = my_font.render(f"F1/F2 = +/- threshold", False, (0, 255, 255))
+    instr2_text = my_font.render(
+        f"F1/F2 = +/- threshold", False, (0, 255, 255))
     screen.blit(dot_text, (300, 300))
     screen.blit(dist_text, (300, 330))
     screen.blit(instr1_text, (300, 400))
     screen.blit(instr2_text, (300, 430))
 
-    if dot_product > VISIBLE_THRESHOLD and dist_bc < VISIBLE_DISTANCE:
+    if dot_product > VISIBLE_THRESHOLD and dist_bob_alice < VISIBLE_DISTANCE:
         warn_text = my_font.render("Bob sees Alice !", False, (0, 255, 0))
         screen.blit(warn_text, (300, 500))
-    else: 
-        warn_text = my_font.render("Bob does not see Alice !", False, (255, 0, 0))
+    else:
+        warn_text = my_font.render(
+            "Bob does not see Alice !", False, (255, 0, 0))
         screen.blit(warn_text, (300, 500))
-    
+
     pygame.display.update()
 
 pygame.quit()
